82b855c135974bbc5d93256c6a0b5c2502f44b26
tenzin <ten13zin@gmail.com>
2018-12-14 20:10:18 +0530

    change default color theme to custom color theme

--- main.py
+++ main.py
@@ -22,10 +22,12 @@ class LabelTool():
         self.parent.title("LabelTool")
         self.frame = Frame(self.parent)
         self.frame.pack(fill=BOTH, expand=1)
+        self.frame.configure(background='light blue')
         self.parent.resizable(width = FALSE, height = FALSE)
 
         # Initialize menu
         self.menubar = Menu(self.parent)
+        self.menubar.configure(background='dark turquoise')
 
         # initialize global state
         self.imageDir = ''
@@ -56,10 +58,12 @@ class LabelTool():
         # dir entry & load
         self.label = Label(self.frame, text = "Image Dir:")
         self.label.grid(row = 0, column = 0, sticky = E)
+        self.label.configure(background='dark turquoise')
         self.entry = Entry(self.frame)
         self.entry.grid(row = 0, column = 1, sticky = W+E)
         self.ldBtn = Button(self.frame, text = "Load", command = self.loadDir)
         self.ldBtn.grid(row = 0, column = 2, sticky = W+E)
+        self.ldBtn.configure(background='green')
 
         # main panel for labeling
         self.mainPanel = Canvas(self.frame, cursor='tcross')
@@ -70,16 +74,20 @@ class LabelTool():
         self.parent.bind("a", self.prevImage) # press 'a' to go backforward
         self.parent.bind("d", self.nextImage) # press 'd' to go forward
         self.mainPanel.grid(row = 1, column = 1, rowspan = 4, sticky = W+N)
+        self.mainPanel.configure(background='light blue')
 
         # showing bbox info & delete bbox
         self.lb1 = Label(self.frame, text = 'Bounding boxes:')
         self.lb1.grid(row = 1, column = 2,  sticky = W+N)
+        self.lb1.configure(background='dark turquoise')
         self.listbox = Listbox(self.frame, width = 22, height = 12)
         self.listbox.grid(row = 2, column = 2, sticky = N)
         self.btnDel = Button(self.frame, text = 'Delete', command = self.delBBox)
         self.btnDel.grid(row = 3, column = 2, sticky = W+E+N)
+        self.btnDel.configure(background='red')
         self.btnClear = Button(self.frame, text = 'ClearAll', command = self.clearBBox)
         self.btnClear.grid(row = 4, column = 2, sticky = W+E+N)
+        self.btnClear.configure(background='red')
 
         # Save to database
         self.dbmenu = Menu(self.menubar, tearoff=0)
@@ -91,18 +99,24 @@ class LabelTool():
         # control panel for image navigation
         self.ctrPanel = Frame(self.frame)
         self.ctrPanel.grid(row = 5, column = 1, columnspan = 2, sticky = W+E)
+        self.ctrPanel.configure(background='medium sea green')
         self.prevBtn = Button(self.ctrPanel, text='<< Prev', width = 10, command = self.prevImage)
         self.prevBtn.pack(side = LEFT, padx = 5, pady = 3)
+        self.prevBtn.configure(background='green')
         self.nextBtn = Button(self.ctrPanel, text='Next >>', width = 10, command = self.nextImage)
         self.nextBtn.pack(side = LEFT, padx = 5, pady = 3)
+        self.nextBtn.configure(background='green')
         self.progLabel = Label(self.ctrPanel, text = "Progress:     /    ")
         self.progLabel.pack(side = LEFT, padx = 5)
+        self.progLabel.configure(background='green')
 
         # example pannel for illustration
         self.egPanel = Frame(self.frame, border = 10)
         self.egPanel.grid(row = 1, column = 0, rowspan = 5, sticky = N)
+        self.egPanel.configure(background='light blue')
         self.tmpLabel2 = Label(self.egPanel, text = "Examples:")
         self.tmpLabel2.pack(side = TOP, pady = 5)
+        self.tmpLabel2.configure(background='dark turquoise')
         self.egLabels = []
         for i in range(3):
             self.egLabels.append(Label(self.egPanel))
